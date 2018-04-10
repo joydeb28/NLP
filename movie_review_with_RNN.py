0557f635788c93158daafbd9f381ddd434b288cb
Joy <joydeb28@gmail.com>
2018-04-10 15:57:00 +0530

    Update

--- movie_review_with_RNN.py
+++ movie_review_with_RNN.py
@@ -1,21 +1,9 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 10 11:52:58 2018
-
-@author: joy
-"""
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 from scipy.spatial.distance import cdist
-'''
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, GRU, Embedding
-from tensorflow.python.keras.optimizers import Adam
-from tensorflow.python.keras.preprocessing.text import Tokenizer
-from tensorflow.python.keras.preprocessing.sequence import pad_sequences
-'''
-#from tf.keras.models import Sequential  # This does not work!
+
 from keras.models import Sequential
 from keras.layers import Dense, GRU, Embedding
 from keras.optimizers import Adam
@@ -91,4 +79,29 @@ model.compile(loss='binary_crossentropy',
               metrics=['accuracy'])
 model.summary()
 model.fit(x_train_pad, y_train,
-          validation_split=0.05, epochs=3, batch_size=64)                       
+          validation_split=0.05, epochs=3, batch_size=64)    
+
+y_pred = model.predict(x=x_test_pad[0:1000])
+y_pred = y_pred.T[0]
+cls_pred = np.array([1.0 if p>0.5 else 0.0 for p in y_pred])
+cls_true = np.array(y_test[0:1000])
+
+text1 = "This movie is fantastic! I really like it because it is so good!"
+text2 = "Good movie!"
+text3 = "Maybe I like this movie."
+text4 = "Meh ..."
+text5 = "If I were a drunk teenager then this movie might be good."
+text6 = "Bad movie!"
+text7 = "Not a good movie!"
+text8 = "This movie really sucks! Can I get my money back please?"
+texts = [text1, text2, text3, text4, text5, text6, text7, text8] 
+
+tokens = tokenizer.texts_to_sequences(texts)
+tokens_pad = pad_sequences(tokens, maxlen=max_tokens,
+                           padding=pad, truncating=pad)
+                           
+model.predict(tokens_pad)
+'''
+A value close to 0.0 means a negative sentiment and a value close to 1.0 means a positive sentiment. 
+These numbers will vary every time you train the model.
+'''                           
